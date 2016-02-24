dc7c1324eaf1dde8c907f56910999a055360fb22
albertmonteiro <albertmonteiro@live.com>
2016-02-24 16:38:38 -0500

    Added movement forces

--- Assets/_Scripts/ChameleonController.cs
+++ Assets/_Scripts/ChameleonController.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 // VELOCITY RANGE UTILITY CLASS ++++++++++++++++++++++++++++
 [System.Serializable]
-public class VelocityRange {
-    //PUBLIC INSTANCE VARIABLES
+public class VelocityRange
+{
+    // PUBLIC INSTANCE VARIABLES ++++
     public float minimum;
     public float maximum;
 
-    // CONSTRUCTOR
-    public VelocityRange(float minimum, float maximum){
+    // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++
+    public VelocityRange(float minimum, float maximum)
+    {
         this.minimum = minimum;
         this.maximum = maximum;
     }
@@ -23,11 +25,11 @@ public class ChameleonController : MonoBehaviour
     public float jumpForce;
     public Transform groundCheck;
 
-    // PRIVATE INSTANCE VARIABLES
+    // PRIVATE  INSTANCE VARIABLES
     private Animator _animator;
     private float _move;
     private float _jump;
-    private bool _facingRight = true;
+    private bool _facingRight;
     private Transform _transform;
     private Rigidbody2D _rigidBody2D;
     private bool _isGrounded;
@@ -35,12 +37,10 @@ public class ChameleonController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        // Initialize public instance variables
-        this.velocityRange = new VelocityRange(300f, 1000f);
-        this.moveForce = 50f;
-        this.jumpForce = 500f;
+        // Initialize Public Instance Variables
+        this.velocityRange = new VelocityRange(300f, 500f);
 
-        // Initial private instance variables
+        // Initialize Private Instance Variables
         this._transform = gameObject.GetComponent<Transform>();
         this._animator = gameObject.GetComponent<Animator>();
         this._rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
@@ -52,35 +52,105 @@ public class ChameleonController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        this._isGrounded = Physics2D.Linecast(this._transform.position, this.groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        this._isGrounded = Physics2D.Linecast(
+            this._transform.position,
+            this.groundCheck.position,
+            1 << LayerMask.NameToLayer("Ground"));
         Debug.DrawLine(this._transform.position, this.groundCheck.position);
 
-        
+
+
 
         float forceX = 0f;
         float forceY = 0f;
 
-        // Absolute value of velocity for gameObject
+        // get absolute value of velocity for our gameObject
         float absVelX = Mathf.Abs(this._rigidBody2D.velocity.x);
         float absVelY = Mathf.Abs(this._rigidBody2D.velocity.y);
 
+        //// Ensure the player is grounded before any movement checks
+        //if (this._isGrounded)
+        //{
+        //    // gets a number between -1 to 1 for both Horizontal and Vertical Axes
+        //    this._move = Input.GetAxis("Horizontal");
+        //    this._jump = Input.GetAxis("Vertical");
 
-        // Chacking if character is grounded
-        if (this._isGrounded)
-        {
-            Debug.Log("Grounded");
-            // gets a number between -1 and 1 for both horizontal and vertical axes
+        //    if (this._move != 0)
+        //    {
+        //        if (this._move > 0)
+        //        {
+        //            // movement force
+        //            if (absVelX < this.velocityRange.maximum)
+        //            {
+        //                forceX = this.moveForce;
+        //            }
+        //            this._facingRight = true;
+        //            this._flip();
+        //        }
+        //        if (this._move < 0)
+        //        {
+        //            // movement force
+        //            if (absVelX < this.velocityRange.maximum)
+        //            {
+        //                forceX = -this.moveForce;
+        //            }
+        //            this._facingRight = false;
+        //            this._flip();
+        //        }
+
+        //        // call the walk clip
+        //        this._animator.SetInteger("AnimationState", 1);
+        //    }
+        //    else
+        //    {
+
+        //        // set default animation state to "idle"
+        //        this._animator.SetInteger("AnimationState", 0);
+        //    }
+
+        //    if (this._jump > 0)
+        //    {
+        //        // jump force
+        //        if (absVelY < this.velocityRange.maximum)
+        //        {
+        //            forceY = this.jumpForce;
+        //        }
+
+        //    }
+        //}
+        //else
+        //{
+        //    // call the "jump" clip
+        //    this._animator.SetInteger("AnimationState", 2);
+        //}
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // DOES NOT Ensure the player is grounded before any movement checks
+        //if (this._isGrounded)
+        //{
+            // gets a number between -1 to 1 for both Horizontal and Vertical Axes
             this._move = Input.GetAxis("Horizontal");
             this._jump = Input.GetAxis("Vertical");
+
             if (this._move != 0)
             {
                 if (this._move > 0)
                 {
+                    // movement force
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = this.moveForce;
+                    }
                     this._facingRight = true;
                     this._flip();
                 }
                 if (this._move < 0)
                 {
+                    // movement force
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = -this.moveForce;
+                    }
                     this._facingRight = false;
                     this._flip();
                 }
@@ -94,18 +164,27 @@ public class ChameleonController : MonoBehaviour
                 // set default animation state to "idle"
                 this._animator.SetInteger("AnimationState", 0);
             }
-        }
-        else
-        {
-            Debug.Log("Not Grounded");
-            if (this._jump > 0)
+
+            if (this._jump > 0 && this._isGrounded)
             {
-                // call the "jump" clip
+                // jump force
+                if (absVelY < this.velocityRange.maximum)
+                {
+                    forceY = this.jumpForce;
+                }
                 this._animator.SetInteger("AnimationState", 2);
             }
-        }
+        //}
+        //else
+        //{
+            // call the "jump" clip
+        //}
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-        
+
+        Debug.Log(forceX);
+        // Apply the forces to the player
+        this._rigidBody2D.AddForce(new Vector2(forceX, forceY));
     }
 
     // PRIVATE METHODS
